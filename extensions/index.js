30c3df9cc0377ec8443861cf05b931274be67f43
will3 <will3.git@gmail.com>
2018-04-17 16:03:39 +1200

    MINOR

--- extensions/index.js
+++ extensions/index.js
@@ -17,103 +17,103 @@ const conversationalForm = cf.ConversationalForm.startTheConversation({
   }
 });
 
-// const card = new CardMessage({
-// 	title: 'title',
-// 	detail: 'detail',
-// 	image: 'http://via.placeholder.com/350x150',
-// 	buttons: [{
-// 		title: 'a',
-// 		onClick: () => {
-// 			alert('a');
-// 		}
-// 	}, {
-// 		title: 'b',
-// 		onClick: () => {
-// 			alert('b');
-// 		}
-// 	}]
-// });
-
-// conversationalForm.addMessage({
-// 	isRobot: true,
-// 	response: 'user thinking not supported',
-// 	responseTime: Infinity
-// });
-
-// conversationalForm.addMessage({
-// 	isRobot: true,
-// 	response: 'card',
-// 	attachment: card
-// });
-
-// conversationalForm.addMessage({
-// 	isRobot: true,
-// 	response: 'gallery',
-// 	attachment: new GalleryMessage({
-// 		cards: [ card, card ]
-// 	})
-// });
+const card = new CardMessage({
+	title: 'title',
+	detail: 'detail',
+	image: 'http://via.placeholder.com/350x150',
+	buttons: [{
+		title: 'a',
+		onClick: () => {
+			alert('a');
+		}
+	}, {
+		title: 'b',
+		onClick: () => {
+			alert('b');
+		}
+	}]
+});
 
 conversationalForm.addMessage({
-	isRobot: true,
+	isRobot: false,
+	response: 'user thinking not supported',
+	responseTime: Infinity
+});
+
+conversationalForm.addMessage({
+	isRobot: false,
+	response: 'card',
+	attachment: card
+});
+
+conversationalForm.addMessage({
+	isRobot: false,
+	response: 'gallery',
+	attachment: new GalleryMessage({
+		cards: [ card, card ]
+	})
+});
+
+conversationalForm.addMessage({
+	isRobot: false,
 	response: 'video',
 	attachment: new VideoMessage({
 		source: 'https://vjs.zencdn.net/v/oceans.mp4'
 	})
 });
 
-// conversationalForm.addMessage({
-// 	isRobot: true,
-// 	response: 'audio',
-// 	attachment: new AudioMessage({
-// 		source: 'https://raw.githubusercontent.com/anars/blank-audio/master/10-minutes-of-silence.mp3'
-// 	})
-// });
-
-// conversationalForm.addMessage({
-// 	isRobot: true,
-// 	response: 'share your current location?',
-// 	attachment: new LocationMessage({
-// 		onLocationSuccess: (position) => {
-// 			console.log(position);
-// 		}
-// 	})
-// });
+conversationalForm.addMessage({
+	isRobot: false,
+	response: 'audio',
+	attachment: new AudioMessage({
+		source: 'https://raw.githubusercontent.com/anars/blank-audio/master/10-minutes-of-silence.mp3'
+	})
+});
 
 conversationalForm.addMessage({
-	isRobot: true,
+	isRobot: false,
+	response: 'share your current location?',
+	attachment: new LocationMessage({
+		onLocationSuccess: (position) => {
+			console.log(position);
+		}
+	})
+});
+
+conversationalForm.addMessage({
+	isRobot: false,
 	response: 'take photo',
 	attachment: new TakePhotoMessage({})
 });
 
 conversationalForm.addMessage({
-	isRobot: true,
+	isRobot: false,
 	response: 'record video',
 	attachment: new RecordVideoMessage({})
 });
 
 conversationalForm.addMessage({
-	isRobot: true,
+	isRobot: false,
 	response: 'record audio',
 	attachment: new RecordAudioMessage({})
 });
 
-// conversationalForm.addTags([{
-// 	tag: "input",
-// 	type: "date",
-// 	"cf-questions": "date?"
-// }]);
+conversationalForm.addTags([{
+	tag: "input",
+	type: "date",
+	"cf-questions": "date?"
+}]);
 
-// conversationalForm.addTags([{
-// 	tag: "input",
-// 	type: "datetime-local",
-// 	"cf-questions": "datetime?"
-// }]);
+conversationalForm.addTags([{
+	tag: "input",
+	type: "datetime-local",
+	"cf-questions": "datetime?"
+}]);
 
-// conversationalForm.addTags([{
-// 	tag: "input",
-// 	type: "time",
-// 	"cf-questions": "time?"
-// }]);
+conversationalForm.addTags([{
+	tag: "input",
+	type: "time",
+	"cf-questions": "time?"
+}]);
 
-// conversationalForm.remapTagsAndStartFrom();
+conversationalForm.remapTagsAndStartFrom();
